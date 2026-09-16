c90da6f406660dbee91dc87a184f94cbe89cc9cd
dieudonne-ai <didopro2027@gmail.com>
2026-09-16 10:44:19 +0300

    My Lecture note for my Year 2 sem 1

---
+++ OOP/Exo/bank.java
@@ -0,0 +1,26 @@
+public class bank {
+
+    public static void main(String[] args) {
+        
+    }
+}
+
+
+class Bank {
+
+    String accountNumber;
+    String accountHolder;
+    double balance;
+
+    void deposit(double amount) {
+        balance = balance + amount;
+    }
+
+    void withdraw(double amount) {
+        balance = balance - amount;
+    }
+
+    void checkBalance() {
+        System.out.println("Balance: " + balance);
+    }
+}
